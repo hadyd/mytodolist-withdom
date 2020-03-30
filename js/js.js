676f81af8fab462397c3c9bbb546ca769e9f0cd5
hadyd <anr.hadyd@gmail.com>
2020-03-30 20:38:26 +0700

    First Update

---
+++ js/js.js
@@ -0,0 +1,7 @@
+function createList() {
+  let li = document.createElement("li");
+  let inputValue = document.getElementById("myInput").value;
+  let t = document.createTextNode(inputValue);
+  li.appendChild(t);
+  document.getElementById("myUL").appendChild(li);
+}
